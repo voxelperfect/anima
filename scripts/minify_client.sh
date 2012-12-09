4c6d452b65399bfa9835dc108ebcb77a1cd6726c
VoxelPerfect <kostas.karolemeas@me.com>
2012-12-09 06:56:03 +0200

    removed absolute path to uglifyjs

--- scripts/minify_client.sh
+++ scripts/minify_client.sh
@@ -12,7 +12,7 @@ rm -rf ../deploy
 mkdir ../deploy
 
 cat ../www/js/anima.js ../www/js/sound.js ../www/js/renderer.js ../www/js/node.js ../www/js/layer.js ../www/js/scene.js ../www/js/easing.js ../www/js/animator.js ../www/js/canvas.js ../www/js/level.js ../www/js/body.js ../www/js/ext/score.js ../www/js/ext/slideshow.js >> anima.js
-/usr/local/lib/node_modules/uglify-js/bin/uglifyjs --max-line-len 1024 --unsafe --output ../deploy/anima.min.js anima.js
+uglifyjs --max-line-len 1024 --unsafe --output ../deploy/anima.min.js anima.js
 cp anima.js ../deploy/
 rm anima.js
 echo "    compressed javascript files"
